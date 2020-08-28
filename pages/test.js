8a0483c6d37c71a88a833369171159aaba3d6a5d
amitkumarpaswan1 <pin2kumar96@gmail.com>
2020-08-28 19:46:14 +0530

    first app

---
+++ pages/test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function Test(){
+
+
+    return (
+        <div>
+            <span>aaaaaaaaaaaaaaaaaaaaaaaaaaaa</span>
+
+        </div>
+
+    );
+}
+
+export default Test;
